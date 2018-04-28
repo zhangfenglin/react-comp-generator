beb8cb9f79ce95dcf626ecac0067115b26464ea3
zhangfenglin <linzi880101>
2018-04-28 21:38:26 +0800

    add commander tool

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const compGenerate = require('./lib/comp-generate')
+const commander = require('commander')
+// const compGenerate = require('./lib/comp-generate')
 
-compGenerate()
+// compGenerate()
+
+const log = (...args) => console.log('gen-code:', ...args)
+
+const pkg = require('./package.json')
+
+commander
+  .version(pkg.version)
+  .option('--init', 'Init Component')
+  .parse(process.argv)
+
+if (commander.init) {
+  log('init ....')
+}
